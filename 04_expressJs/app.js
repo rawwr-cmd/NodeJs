024ceabc1a1a7e2c5b08175816075a6cabebbef5
rawwr <akshayace0302@gmail.com>
2022-07-25 22:33:59 +0530

    adding 404

--- 04_expressJs/app.js
+++ 04_expressJs/app.js
@@ -10,7 +10,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-app;
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(8080, () => {
   console.log("listening on port 8080");
