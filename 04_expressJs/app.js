c402b51df0e23febc9e3167fdb8deb31decc34ff
rawwr <akshayace0302@gmail.com>
2022-07-25 21:37:12 +0530

    body-parser

--- 04_expressJs/app.js
+++ 04_expressJs/app.js
@@ -1,18 +1,36 @@
-const http = require("http");
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 
-app.use((req, res, next) => {
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", (req, res, next) => {
+  //   console.log("a middleware for all routes");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
   //   console.log("we got a new request");
   //   console.dir(req);
   //   res.send("HELLO, WE GOT YOUR REQUEST");
   //   res.send({ color: "red" });
-  console.log("IN THE MIDDLEWARE");
-  next(); //allows the request to continue to the next middleware in line
+  //   console.log("IN THE MIDDLEWARE");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="product"><button type="submit">Add Product</button></form>'
+  );
+  //   next(); //allows the request to continue to the next middleware in line
 });
 
-app.use((req, res, next) => {
-  console.log("In the another middleware");
+app.use("/product", (req, res, next) => {
+  //   console.log("In another middleware");
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>The home page</h1>");
+  //   console.log("In the another middleware");
+  //...
 });
 
 app.listen(8080, () => {
