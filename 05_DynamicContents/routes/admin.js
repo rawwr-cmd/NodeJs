513df6b946fe7890e2e05bbff1050503d42c498d
rawwr <akshayace0302@gmail.com>
2022-07-29 22:09:01 +0530

    converting the html into pug

--- 05_DynamicContents/routes/admin.js
+++ 05_DynamicContents/routes/admin.js
@@ -2,14 +2,15 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+// const rootDir = require("../util/path");
 
 const router = express.Router();
 const products = [];
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", { pageTitle: "Add Product" });
 });
 
 // /admin/add-product => POST
