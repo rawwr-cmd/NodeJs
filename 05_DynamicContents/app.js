54a1a4fa9e210f2527b16e04588bf7f6f31a8b0a
rawwr <akshayace0302@gmail.com>
2022-07-30 01:07:30 +0530

    partialing ejs

--- 05_DynamicContents/app.js
+++ 05_DynamicContents/app.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
+// app.set("view engine", "pug");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
