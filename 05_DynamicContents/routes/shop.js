9b6670ef887d8e96f95d98def67e74e9dec44bb8
rawwr <akshayace0302@gmail.com>
2022-07-29 23:26:54 +0530

    finishing the pug template

--- 05_DynamicContents/routes/shop.js
+++ 05_DynamicContents/routes/shop.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+// const rootDir = require("../util/path");
 const adminData = require("./admin");
 const router = express.Router();
 
@@ -10,7 +10,7 @@ router.get("/", (req, res, next) => {
   // console.log("shop.js", adminData.products);
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
   const products = adminData.products;
-  res.render("shop", { prods: products, docTitle: "Shop" });
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
