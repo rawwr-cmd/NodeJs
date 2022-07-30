f64b0687f55c9b6d9cd80caedd73012b8d3ed9ff
rawwr <akshayace0302@gmail.com>
2022-07-30 21:42:23 +0530

    adding order template

--- 05_DynamicContents/routes/shop.js
+++ 05_DynamicContents/routes/shop.js
@@ -1,12 +1,12 @@
 const path = require("path");
 
 const express = require("express");
-
 const {
   getProducts,
   getIndex,
   getCart,
   getCheckout,
+  getOrders,
 } = require("../controllers/shop");
 
 const router = express.Router();
@@ -14,6 +14,7 @@ const router = express.Router();
 router.get("/", getIndex);
 router.get("/products", getProducts);
 router.get("/cart", getCart);
+router.get("/orders", getOrders);
 router.get("/checkout", getCheckout);
 
 module.exports = router;
