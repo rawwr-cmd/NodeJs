c1bf36142c1afb343be98158717e9f4d7e3f07a4
rawwr <akshayace0302@gmail.com>
2022-07-30 21:20:52 +0530

    adding routes for delete and edit

--- 05_DynamicContents/models/product.js
+++ 05_DynamicContents/models/product.js
@@ -1,6 +1,7 @@
 // const products = [];
 const fs = require("fs");
 const path = require("path");
+
 const p = path.join(
   path.dirname(require.main.filename),
   "data",
@@ -18,9 +19,13 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
+
   save() {
     getProductsFromFile((products) => {
       products.push(this);
