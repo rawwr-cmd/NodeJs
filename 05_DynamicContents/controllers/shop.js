d7fc26576c4fb0deb5e832bf12dd4cdbc4daf9ef
rawwr <akshayace0302@gmail.com>
2022-08-23 03:46:10 +0530

    finishing the cart model

--- 05_DynamicContents/controllers/shop.js
+++ 05_DynamicContents/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   //   console.log("shop.js", adminData.products);
@@ -45,7 +46,9 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const { productId } = req.body;
-  console.log(productId);
+  Product.findById(productId, (product) => {
+    Cart.addProduct(productId, product.price);
+  });
   res.redirect("/cart");
 };
 
--- 05_DynamicContents/models/cart.js
+++ 05_DynamicContents/models/cart.js
@@ -4,29 +4,44 @@ const path = require("path");
 const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
 
 module.exports = class Cart {
-  static addProduct(id) {
+  // fetch the previous cart
+  static addProduct(id, productPrice) {
+    //passing the path with the callback
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
       if (!err) {
         cart = JSON.parse(fileContent);
       }
       //finding the existing product
-      const existingProduct = cart.products.find(
+      //if the product id matches the id we are  passing then we will add one more to the quantity
+      const existingProductIndex = cart.products.findIndex(
         (product) => product.id === id
       );
+
+      // console.log(existingProductIndex);
+      const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
-      //for existing product
+      //add new product/ increase quantity
       if (existingProduct) {
         updatedProduct = { ...existingProduct };
         updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
         //for updated product
       } else {
         updatedProduct = { id, qty: 1 };
-        Cart.products = [...cart.products];
+        cart.products = [...cart.products, updatedProduct];
+        console.log(cart.products);
       }
-      Cart.totalPrice += productPrice;
-
-      updatedProduct;
+      cart.totalPrice += +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
     });
   }
 };
+
+//const fruits = ["apple", "banana", "cantaloupe", "blueberries", "grapefruit"];
+// const index = fruits.findIndex((fruit) => fruit === "blueberries");
+// console.log(index); // 3
+// console.log(fruits[index]); // blueberries
