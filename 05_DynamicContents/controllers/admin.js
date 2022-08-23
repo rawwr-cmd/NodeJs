9278961e709feb5dc7813f2198186d0171c0d285
rawwr <akshayace0302@gmail.com>
2022-08-23 05:54:34 +0530

    prepopulating the edit product page with data

--- 05_DynamicContents/controllers/admin.js
+++ 05_DynamicContents/controllers/admin.js
@@ -1,10 +1,10 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
+    editing: "false",
   });
 };
 
@@ -16,23 +16,23 @@ exports.postAddProduct = (req, res, next) => {
   res.redirect("/");
 };
 
-exports.getAddProduct = (req, res, next) => {
-  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
-  res.render("admin/edit-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-  });
-};
-
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
     return res.redirect("/");
   }
-  res.render("admin/edit-product", {
-    pageTitle: "Edit Product",
-    path: "/admin/edit-product",
-    editing: editMode,
+  //req params actually grabs the id from the url
+  const { productId } = req.params;
+  Product.findById(productId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
+    });
   });
 };
 
