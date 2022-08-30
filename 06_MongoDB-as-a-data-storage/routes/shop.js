4b7905a0065b07730a2adfcad4408e8a7b1a2fd2
rawwr <akshayace0302@gmail.com>
2022-08-30 05:50:19 +0530

    idk

--- 06_MongoDB-as-a-data-storage/routes/shop.js
+++ 06_MongoDB-as-a-data-storage/routes/shop.js
@@ -18,7 +18,7 @@ const router = express.Router();
 router.get("/", getIndex);
 router.get("/products", getProducts);
 router.get("/products/:productId", getProductDetail);
-router.get("/cart", getCart);
+// router.get("/cart", getCart);
 router.post("/cart", postCart);
 // router.post("/cart-delete-item", postCartDeleteProduct);
 // router.post("/create-order", postOrder);
