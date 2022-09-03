829425bcab3906ced977469fbceb056a3c22ce11
rawwr <akshayace0302@gmail.com>
2022-09-03 21:06:27 +0530

    initial setup and adding login page

---
+++ 08_cookies-and-sessions/app.js
@@ -0,0 +1,67 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const { get404 } = require("./controllers/error");
+const User = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+// app.set("view engine", "pug");
+app.set("views", "views");
+
+//routes
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("6311c656b7aeedb3de7112cb")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(get404);
+
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "rawwr",
+          email: "rawwr69@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000, () => {
+      console.log("The application is running on localhost 3000");
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
---
+++ 08_cookies-and-sessions/routes/shop.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+const express = require("express");
+
+const {
+  getProducts,
+  getIndex,
+  getCart,
+  getOrders,
+  getProductDetail,
+  postCart,
+  postCartDeleteProduct,
+  postOrder,
+} = require("../controllers/shop");
+
+const router = express.Router();
+
+router.get("/", getIndex);
+router.get("/products", getProducts);
+router.get("/products/:productId", getProductDetail);
+router.get("/cart", getCart);
+router.post("/cart", postCart);
+router.post("/cart-delete-item", postCartDeleteProduct);
+router.post("/create-order", postOrder);
+router.get("/orders", getOrders);
+
+module.exports = router;
