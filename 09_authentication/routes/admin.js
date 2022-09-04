238c06c0902214c2371eee5fadde78610faa5c68
rawwr <akshayace0302@gmail.com>
2022-09-05 00:27:07 +0530

    protecting routes using middleware

--- 09_authentication/routes/admin.js
+++ 09_authentication/routes/admin.js
@@ -10,20 +10,22 @@ const {
   postDeleteProduct,
 } = require("../controllers/admin");
 
+const isAuth = require("../middleware/is-auth");
+
 const router = express.Router();
 
 // // /admin/add-product => GET
-router.get("/add-product", getAddProduct);
-router.get("/products", getProducts);
+router.get("/add-product", isAuth, getAddProduct);
+router.get("/products", isAuth, getProducts);
 
 // // /admin/add-product => POST
-router.post("/add-product", postAddProduct);
+router.post("/add-product", isAuth, postAddProduct);
 
 // // /admin/edit-product/:productId => GET
-router.get("/edit-product/:productId", getEditProduct);
+router.get("/edit-product/:productId", isAuth, getEditProduct);
 
-router.post("/edit-product", postEditProduct);
+router.post("/edit-product", isAuth, postEditProduct);
 
-router.post("/delete-product", postDeleteProduct);
+router.post("/delete-product", isAuth, postDeleteProduct);
 
 module.exports = router;
--- 09_authentication/routes/shop.js
+++ 09_authentication/routes/shop.js
@@ -13,15 +13,17 @@ const {
   postOrder,
 } = require("../controllers/shop");
 
+const isAuth = require("../middleware/is-auth");
+
 const router = express.Router();
 
 router.get("/", getIndex);
 router.get("/products", getProducts);
 router.get("/products/:productId", getProductDetail);
-router.get("/cart", getCart);
-router.post("/cart", postCart);
-router.post("/cart-delete-item", postCartDeleteProduct);
-router.post("/create-order", postOrder);
-router.get("/orders", getOrders);
+router.get("/cart", isAuth, getCart);
+router.post("/cart", isAuth, postCart);
+router.post("/cart-delete-item", isAuth, postCartDeleteProduct);
+router.post("/create-order", isAuth, postOrder);
+router.get("/orders", isAuth, getOrders);
 
 module.exports = router;
