1f5b39be9f608158ce18602da5ebc2e02eb3e70a
rawwr <akshayace0302@gmail.com>
2022-09-08 05:24:52 +0530

    serving files statically

--- 13_FileUpload-download-and-pagination/app.js
+++ 13_FileUpload-download-and-pagination/app.js
@@ -75,6 +75,8 @@ app.use(
 );
 
 app.use(express.static(path.join(__dirname, "public")));
+//serving images statically
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(session(sessionConfig));
 app.use(csrfProtection);
