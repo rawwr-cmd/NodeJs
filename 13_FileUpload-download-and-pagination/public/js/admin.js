1024e67499d260eb5e6618d53a0fe8357025e607
rawwr <akshayace0302@gmail.com>
2022-09-10 17:48:03 +0530

    adding client side js code

---
+++ 13_FileUpload-download-and-pagination/public/js/admin.js
@@ -0,0 +1,5 @@
+const deleteProduct = (btn) => {
+  //   console.log(btn);
+  const prodId = btn.parentNode.querySelector("[name=productId]").value;
+  const crsf = btn.parentNode.querySelector("[name=_csrf]").value;
+};
