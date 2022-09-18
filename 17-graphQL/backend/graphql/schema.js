89b20f68545049e3ba203efe55b3f12cdb0427e3
rawwr <akshayace0302@gmail.com>
2022-09-18 20:13:30 +0530

    defining a mutation

--- 17-graphQL/backend/graphql/schema.js
+++ 17-graphQL/backend/graphql/schema.js
@@ -1,16 +1,36 @@
 const { buildSchema } = require("graphql");
 
 module.exports = buildSchema(`
- type PERSON { 
-    message: String!
-    views: Int!
- }
+   type Post {
+      _id: ID!
+      title: String!
+      content: String!
+      imageUrl: String!
+      creator: User!
+      createdAt: String!
+      updatedAt: String!
+   }
 
- type rootQuery {
-    hello: PERSON!
-    }
+   type User {
+      _id: ID!
+      name: String!
+      email: String!
+      password: String
+      status: String!
+      posts: [Post!]!
+   }
 
- schema {         
-   query: rootQuery
- }
+   Input UserInputData {
+      email: String!
+      name: String!
+      password: String!
+   }
+
+  type RootMutation {
+      createUser(userInput: UserInputData): User!
+  }
+
+  schema {
+    query: RootQuery
+  }
 `);
