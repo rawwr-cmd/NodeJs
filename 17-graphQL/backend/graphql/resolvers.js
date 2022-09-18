c852e93b1045cf140842e0d4b6e770d46958b8eb
rawwr <akshayace0302@gmail.com>
2022-09-18 22:51:53 +0530

    handling errors

--- 17-graphQL/backend/graphql/resolvers.js
+++ 17-graphQL/backend/graphql/resolvers.js
@@ -1,9 +1,29 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 module.exports = {
   createUser: async ({ userInput }, req) => {
     // const email = args.userInput.email;
+
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: "E-Mail is invalid." });
+    }
+
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: "Password too short!" });
+    }
+
+    if (errors.length > 0) {
+      const error = new Error("Invalid input.");
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
 
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
